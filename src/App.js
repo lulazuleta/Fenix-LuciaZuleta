e87bca6650a58fdeca8065501d5fd12b6c15d85f
lulazuleta <lucia.lzuleta@gmail.com>
2021-06-04 20:01:14 -0300

    Primer entrega

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         Proyecto Casa Fenix
+        </p>
+        <a
+          className="App-link"
+          href="https://casafenix.com.ar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visitá nuestra web actual
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
